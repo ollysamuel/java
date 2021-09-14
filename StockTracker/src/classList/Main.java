b32b4023c2c64d971f148de1b8d92425a282ac70
ollysamuel <ollydrummer@gmail.com>
2021-09-14 21:59:38 +0100

    initial stock tracker commit

---
+++ StockTracker/src/classList/Main.java
@@ -0,0 +1,40 @@
+package classList;
+
+import java.util.*;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		// create scanner keyboard objects
+		Scanner keyboard = new Scanner(System.in);
+		Scanner keyboardString = new Scanner(System.in);
+		
+		// create array list of stock item objects
+		ArrayList<StockItem>StockList = new ArrayList<>();
+		
+		// declare variables
+		String tempNumber, tempName;
+		double tempPrice;
+		
+		// input item details
+		System.out.print("Enter the stock number: ");
+		tempNumber = keyboardString.nextLine();
+		System.out.print("Enter the name of the item: ");
+		tempName = keyboardString.nextLine();
+		System.out.print("Enter the price of the item: ");
+		tempPrice = keyboard.nextDouble();
+		
+		// Create a new item of stock using the values that were entered by the user
+		StockList.add(new StockItem(tempNumber, tempName, tempPrice));
+		
+		// Increase the total number of items in stock by 5
+		StockList.get(0).increaseTotalStock(5);
+		
+		// Display the stock number
+		System.out.println("Stock Number: " + StockList.get(0).getStockNumber());
+		
+		// Display the total price of all items in stock
+		System.out.println("Total price: £" + StockList.get(0).calculateTotalPrice());
+	}
+}
---
+++ StockTracker/src/classList/StockItem.java
@@ -0,0 +1,55 @@
+package classList;
+
+public class StockItem 
+{
+	// declare variables
+	private String stockNumber, name;
+	private double price;
+	private int totalStock = 0;
+	
+	// constructor
+	public StockItem(String stockNumIn, String nameIn, double priceIn)
+	{
+		stockNumber = stockNumIn;
+		name = nameIn;
+		price = priceIn;
+	}
+	
+	// class methods
+	
+	public void setPrice(double priceIn)
+	{
+		price = priceIn;
+	}
+	
+	public void increaseTotalStock(int stockIn)
+	{
+		totalStock += stockIn;
+	}
+	
+	public String getStockNumber()
+	{
+		return stockNumber;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getTotalStock()
+	{
+		return totalStock;
+	}
+	
+	public double getPrice()
+	{
+		return price;
+	}
+	
+	public double calculateTotalPrice()
+	{
+		return totalStock * price;
+	}
+
+}

91801f466d9d7adb1d6da877b72a04e729834538
ollysamuel <ollydrummer@gmail.com>
2021-09-17 23:55:52 +0100

    initial apartment commit

---
+++ RoomSizer/src/classFiles/Apartment.java
@@ -0,0 +1,55 @@
+package classFiles;
+
+import java.util.ArrayList;
+
+public class Apartment 
+{
+	private static ArrayList<Oblong>rooms = new ArrayList<>();
+	
+	// methods
+	public void addRoom(double lengthIn, double widthIn)
+	{
+		if (lengthIn <= 0 || widthIn <= 0)
+			System.out.println("ERROR: Please enter a positive number");
+		else
+			rooms.add(new Oblong(lengthIn, widthIn));
+	}
+	
+	public double getRoomArea(int roomIn)
+	{
+		roomIn -= 1;
+		if (roomExists(roomIn))
+			return rooms.get(roomIn).calculateArea();
+		else
+			return 0.0;
+	}
+	
+	public double getRoomLength(int roomIn)
+	{
+		roomIn -= 1;
+		if (roomExists(roomIn))
+			return rooms.get(roomIn).getLength();
+		else
+			return 0.0;
+	}
+	
+	public double getRoomWidth(int roomIn)
+	{
+		roomIn -= 1;
+		if (roomExists(roomIn))
+			return rooms.get(roomIn).getHeight();
+		else
+			return 0.0;
+	}
+	
+	static boolean roomExists(int roomNum)
+	{
+		if (roomNum < 1 || roomNum > rooms.size())
+		{
+			System.out.println("ERROR: Room does not exist");
+			return false;
+		}
+		else
+			return true;
+	}
+}
---
+++ RoomSizer/src/classFiles/Main.java
@@ -0,0 +1,61 @@
+package classFiles;
+
+import java.util.*;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		// variables
+		int choice, room;
+		
+		// create objects
+		Apartment apart1 = new Apartment();
+		Scanner keyboard = new Scanner(System.in);
+
+		do
+		{
+			// display menu
+			System.out.println("Room Sizer");
+			System.out.println();
+			System.out.println("What would you like to do?");
+			System.out.println("[1] Add a room");
+			System.out.println("[2] Get room area");
+			System.out.println("[3] Get room width");
+			System.out.println("[4] Get room length");
+			System.out.println("[5] Exit");
+			System.out.println();
+			
+			// get user choice
+			System.out.print("Please enter a number: ");
+			choice = keyboard.nextInt();
+
+			switch (choice) 
+			{
+				case 1: apart1.addRoom(enterDimension("length: "), enterDimension("width: "));
+					break;
+				case 2: System.out.print("What room area would you like?: ");
+						System.out.println(apart1.getRoomArea(keyboard.nextInt()) + "m^2");
+					break;
+				case 3: System.out.print("What room width would you like?: ");
+						System.out.println(apart1.getRoomWidth(keyboard.nextInt()) + "m");
+					break;
+				case 4: System.out.print("What room length would you like?: ");
+						System.out.println(apart1.getRoomLength(keyboard.nextInt()) + "m");
+				case 5: return;
+				default: System.out.println();
+						 System.out.println("Please enter a number 1-4");
+				}
+			System.out.println();
+		} while (choice != 4);
+	}
+	
+	static double enterDimension(String dimensionIn)
+	{
+		double tempDim = 0;
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter " + dimensionIn + ": ");
+		tempDim = keyboard.nextDouble();
+		return tempDim;
+	}
+}
